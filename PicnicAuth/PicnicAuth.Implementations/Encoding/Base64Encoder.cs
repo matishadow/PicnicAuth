368158535f223750843f9d21da12781da34f7676
matishadow <matishadow@gmail.com>
2017-07-24 21:51:33 +0200

    add test for Base64Encoder

--- PicnicAuth/PicnicAuth.Implementations/Encoding/Base64Encoder.cs
+++ PicnicAuth/PicnicAuth.Implementations/Encoding/Base64Encoder.cs
@@ -7,13 +7,15 @@ namespace PicnicAuth.Implementations.Encoding
     {
         public string Encode(string plainText)
         {
+            if (plainText == null) return null;
+
             byte[] plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
             return Encode(plainTextBytes);
         }
 
         public string Encode(byte[] textBytes)
         {
-            return Convert.ToBase64String(textBytes);
+            return textBytes == null ? null : Convert.ToBase64String(textBytes);
         }
     }
 }
---
+++ PicnicAuth/PicnicAuth.Tests/Base64EncoderTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NUnit.Framework;
+using PicnicAuth.Implementations.Encoding;
+
+namespace PicnicAuth.Tests
+{
+    [TestFixture]
+    public class Base64EncoderTest
+    {
+        private Base64Encoder encoder;
+
+        [SetUp]
+        public void Init()
+        {
+            encoder = new Base64Encoder();
+        }
+
+        [TestCase("aaaaaa", ExpectedResult = "YWFhYWFh")]
+        [TestCase("ąćźż", ExpectedResult = "xIXEh8W6xbw=")]
+        [TestCase(null, ExpectedResult = null)]
+        [TestCase("", ExpectedResult = "")]
+        public string TestEncode(string input)
+        {
+            return encoder.Encode(input);
+        }
+
+        [TestCase(new byte[] {0x61, 0x61, 0x61, 0x61, 0x61, 0x61}, ExpectedResult = "YWFhYWFh")]
+        [TestCase(new byte[] {0xc4, 0x85, 0xc4, 0x87, 0xc5, 0xba, 0xc5, 0xbc}, ExpectedResult = "xIXEh8W6xbw=")]
+        [TestCase(null, ExpectedResult = null)]
+        [TestCase(new byte[]{}, ExpectedResult = "")]
+        public string TestEncode(byte[] input)
+        {
+            return encoder.Encode(input);
+        }
+    }
+}
