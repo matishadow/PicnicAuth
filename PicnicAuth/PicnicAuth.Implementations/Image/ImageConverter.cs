73629a8458786f4b96058487773bbc8ada576855
matishadow <matishadow@gmail.com>
2017-07-28 14:19:21 +0200

    add conversion code

---
+++ PicnicAuth/PicnicAuth.Implementations/Image/ImageConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using PicnicAuth.Interfaces.Image;
+
+namespace PicnicAuth.Implementations.Image
+{
+    public class ImageConverter : IImageConverter
+    {
+        public System.Drawing.Image ConvertBitmapToPng(Bitmap bitmap)
+        {
+            if (bitmap == null) throw new ArgumentNullException();
+
+            using (var stream = new MemoryStream())
+            {
+                bitmap.Save(stream, ImageFormat.Png);
+                return System.Drawing.Image.FromStream(stream);
+            }
+        }
+    }
+}
