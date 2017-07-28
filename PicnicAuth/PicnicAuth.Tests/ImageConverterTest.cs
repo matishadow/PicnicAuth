e91a73ce6b712f935a30bf8c0524ee2c675d872f
matishadow <matishadow@gmail.com>
2017-07-28 14:29:13 +0200

    add tests for bitmap to png convertion

---
+++ PicnicAuth/PicnicAuth.Tests/ImageConverterTest.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using NUnit.Framework;
+using PicnicAuth.Implementations.Encoding;
+using PicnicAuth.Interfaces.Image;
+using ImageConverter = PicnicAuth.Implementations.Image.ImageConverter;
+
+namespace PicnicAuth.Tests
+{
+    [TestFixture]
+    public class ImageConverterTest
+    {
+        private IImageConverter converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            converter = new ImageConverter();
+        }
+
+        [Test]
+        public void TestConvertBitmapToPngNullArgument()
+        {
+            Assert.That(() => converter.ConvertBitmapToPng(null),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void TestConvertBitmapToPng()
+        {
+            Bitmap bitmap = CreateDummyBitmap();
+
+            Image pngImage = converter.ConvertBitmapToPng(bitmap);
+
+            Assert.True(Equals(pngImage.RawFormat, ImageFormat.Png));
+        }
+
+        private Bitmap CreateDummyBitmap()
+        {
+            const int size = 50;
+
+            return new Bitmap(size, size);
+        }
+    }
+}
