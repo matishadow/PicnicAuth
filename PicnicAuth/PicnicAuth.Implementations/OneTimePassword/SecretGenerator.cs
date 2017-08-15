6d9405d6549841f3bb9a48bc27a8ecdae3d2e699
matishadow <matishadow@gmail.com>
2017-08-15 17:36:58 +0200

    add tests for secret generator

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/SecretGenerator.cs
@@ -0,0 +1,12 @@
+﻿using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class SecretGenerator : ISecretGenerator
+    {
+        public string GenerateSecret()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Tests/SecretGeneratorTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+using PicnicAuth.Implementations.Encoding;
+using PicnicAuth.Implementations.OneTimePassword;
+using PicnicAuth.Interfaces.Encoding;
+using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Tests
+{
+    [TestFixture]
+    public class SecretGeneratorTest
+    {
+        private ISecretGenerator generator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            generator = new SecretGenerator();
+        }
+
+        [TestCase(ExpectedResult = "JBSWY3DPEHPK3PXP")]
+        public string TestGenerateSecret()
+        {
+            return generator.GenerateSecret();
+        }
+    }
+}
