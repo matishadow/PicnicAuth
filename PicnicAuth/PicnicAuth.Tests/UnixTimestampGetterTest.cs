b1584b1cfaf3fa1dcc9c0dcf5a4ca0efaa3dca9c
matishadow <matishadow@gmail.com>
2017-08-16 22:59:39 +0200

    Add test for getting current timestamp

---
+++ PicnicAuth/PicnicAuth.Tests/UnixTimestampGetterTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using PicnicAuth.Implementations.Encoding;
+using PicnicAuth.Implementations.Time;
+using PicnicAuth.Interfaces.Encoding;
+using PicnicAuth.Interfaces.Time;
+
+namespace PicnicAuth.Tests
+{
+    [TestFixture]
+    public class UnixTimestampGetterTest
+    {
+        private IUnixTimestampGetter unixTimestampGetter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            unixTimestampGetter = new UnixTimestampGetter();
+        }
+
+        [Test]
+        public void TestGetUnixTimestamp()
+        {
+            long currentTimestamp = (long)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+
+            Assert.AreEqual(currentTimestamp, unixTimestampGetter.GetUnixTimestamp());
+        }
+    }
+}
