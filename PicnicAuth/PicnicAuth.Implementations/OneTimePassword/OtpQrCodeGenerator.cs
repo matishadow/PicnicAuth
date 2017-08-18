70fd2e7ac290c2b5888c54879781ba2325a1c37f
matishadow <matishadow@gmail.com>
2017-08-18 11:39:06 +0200

    Add OtpQrCode stub

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/OtpQrCodeGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Drawing;
+using PicnicAuth.Enums;
+using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class OtpQrCodeGenerator : IOtpQrCodeGenerator
+    {
+        public Bitmap GenerateOtpQrCode(string issuer, string user, string secret, OtpType otpType = OtpType.Totp)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Interfaces/OneTimePassword/IOtpQrCodeGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System.Drawing;
+using PicnicAuth.Enums;
+
+namespace PicnicAuth.Interfaces.OneTimePassword
+{
+    public interface IOtpQrCodeGenerator
+    {
+        Bitmap GenerateOtpQrCode(string issuer, string user, string secret, OtpType otpType = OtpType.Totp);
+    }
+}
