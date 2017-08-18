8e9806c727b1830256e2039b1fe2a7a5bce52270
matishadow <matishadow@gmail.com>
2017-08-18 15:34:59 +0200

    Fullfil ArgumentNull test

--- PicnicAuth/PicnicAuth.Implementations/Cryptography/Signature/HmacSha1Generator.cs
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Signature/HmacSha1Generator.cs
@@ -1,4 +1,5 @@
-﻿using PicnicAuth.Interfaces.Cryptography.Signature;
+﻿using System;
+using PicnicAuth.Interfaces.Cryptography.Signature;
 
 namespace PicnicAuth.Implementations.Cryptography.Signature
 {
@@ -6,7 +7,10 @@ namespace PicnicAuth.Implementations.Cryptography.Signature
     {
         public byte[] GenerateHmacSha1Hash(string input, byte[] key)
         {
-            throw new System.NotImplementedException();
+            if (input == null || key == null) 
+                throw new ArgumentNullException();
+
+            throw new NotImplementedException();
         }
     }
 }
