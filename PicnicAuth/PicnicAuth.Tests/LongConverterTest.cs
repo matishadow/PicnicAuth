06f46045721dbbcf52c1b50b36fc6a2b317229d4
matishadow <matishadow@gmail.com>
2017-08-18 16:19:48 +0200

    Add tests for LongConverter

---
+++ PicnicAuth/PicnicAuth.Tests/LongConverterTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NUnit.Framework;
+using PicnicAuth.Implementations.Encoding;
+using PicnicAuth.Interfaces.Encoding;
+
+namespace PicnicAuth.Tests
+{
+    [TestFixture]
+    public class LongConverterTest
+    {
+        private ILongConverter converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            converter = new LongConverter();
+        }
+
+        [TestCase(10000, ExpectedResult = new byte[] { 0x27, 0x10 })]
+        [TestCase(0, ExpectedResult = new byte[] {0})]
+        [TestCase(long.MaxValue, 
+            ExpectedResult = new byte[] {0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff })]
+        public byte[] TestConvertToBytes(long input)
+        {
+            return converter.ConvertToBytes(input);
+        }
+    }
+}
