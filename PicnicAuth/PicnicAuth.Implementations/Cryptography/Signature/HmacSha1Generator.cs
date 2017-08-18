9c7ea90197b6a25a3b952bc7fea0e572918a86ad
matishadow <matishadow@gmail.com>
2017-08-18 15:14:12 +0200

    Add HmacSha1Generator stub

---
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Signature/HmacSha1Generator.cs
@@ -0,0 +1,12 @@
+﻿using PicnicAuth.Interfaces.Cryptography.Signature;
+
+namespace PicnicAuth.Implementations.Cryptography.Signature
+{
+    public class HmacSha1Generator : IHmacSha1Generator
+    {
+        public byte[] GenerateHmacSha1Hash(string input, byte[] key)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Interfaces/Cryptography/Signature/IHmacSha1Generator.cs
@@ -0,0 +1,7 @@
+﻿namespace PicnicAuth.Interfaces.Cryptography.Signature
+{
+    public interface IHmacSha1Generator
+    {
+        byte[] GenerateHmacSha1Hash(string input, byte[] key);
+    }
+}
