28b7ca5dd89709fac853d5326a7b426b86a89af0
matishadow <matishadow@gmail.com>
2017-08-18 16:06:04 +0200

    Add test for other int input and byte[] input methods

--- PicnicAuth/PicnicAuth.Implementations/Cryptography/Signature/HmacSha1Generator.cs
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Signature/HmacSha1Generator.cs
@@ -1,19 +1,46 @@
 ﻿using System;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using PicnicAuth.Interfaces.Cryptography.Signature;
 
 namespace PicnicAuth.Implementations.Cryptography.Signature
 {
     public class HmacSha1Generator : IHmacSha1Generator
     {
-        public byte[] GenerateHmacSha1Hash(string input, byte[] key)
+        public byte[] GenerateHmacSha1Hash(byte[] input, byte[] key)
         {
-            if (input == null || key == null) 
-                throw new ArgumentNullException();
-            if (!key.Any())
-                throw new ArgumentException(Properties.Resources.HmacSha1EmptyKeyExceptionMessage);
+            CheckArguments(input, key);
+            var hmacsha1 = new HMACSHA1(key);
+
+            return hmacsha1.ComputeHash(input);
+        }
+
+        public byte[] GenerateHmacSha1Hash(long input, byte[] key)
+        {
+            CheckArguments(input, key);
 
             throw new NotImplementedException();
+        }
+
+        public byte[] GenerateHmacSha1Hash(string input, byte[] key)
+        {
+            CheckArguments(input, key);
+
+            throw new NotImplementedException();
+        }
+
+        private ArgumentException GenerateEmptyKeyException()
+        {
+            return new ArgumentException(Properties.Resources.HmacSha1EmptyKeyExceptionMessage);
+        }
+
+        private void CheckArguments(object input, byte[] key)
+        {
+            if (input == null || key == null)
+                throw new ArgumentNullException();
+            if (!key.Any())
+                throw GenerateEmptyKeyException();
         }
     }
 }
