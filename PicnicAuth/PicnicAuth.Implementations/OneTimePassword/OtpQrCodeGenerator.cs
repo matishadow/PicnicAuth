50817d348408fb40c4969318b4bd8fb76e6b1811
matishadow <matishadow@gmail.com>
2017-08-18 11:49:58 +0200

    Implement OtpQrCode

--- PicnicAuth/PicnicAuth.Implementations/OneTimePassword/OtpQrCodeGenerator.cs
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/OtpQrCodeGenerator.cs
@@ -1,14 +1,28 @@
 ﻿using System.Drawing;
 using PicnicAuth.Enums;
+using PicnicAuth.Interfaces.Encoding;
 using PicnicAuth.Interfaces.OneTimePassword;
 
 namespace PicnicAuth.Implementations.OneTimePassword
 {
     public class OtpQrCodeGenerator : IOtpQrCodeGenerator
     {
+        private readonly IQrCodeGenerator qrCodeGenerator;
+        private readonly IKeyUriCreator keyUriCreator;
+
+        public OtpQrCodeGenerator(IQrCodeGenerator qrCodeGenerator, IKeyUriCreator keyUriCreator)
+        {
+            this.qrCodeGenerator = qrCodeGenerator;
+            this.keyUriCreator = keyUriCreator;
+        }
+
         public Bitmap GenerateOtpQrCode(string issuer, string user, string secret, OtpType otpType = OtpType.Totp)
         {
-            throw new System.NotImplementedException();
+            string keyUri = keyUriCreator.CreateKeyUri(issuer, user, secret, otpType);
+
+            Bitmap qrCode = qrCodeGenerator.GenerateQrCode(keyUri);
+
+            return qrCode;
         }
     }
 }
