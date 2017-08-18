7eea21d38e7845ae42e1d773f0be8ebe1e828383
matishadow <matishadow@gmail.com>
2017-08-18 15:48:56 +0200

    Add more convienet methods to IHmacSha1Generator interface

--- PicnicAuth/PicnicAuth.Interfaces/Cryptography/Signature/IHmacSha1Generator.cs
+++ PicnicAuth/PicnicAuth.Interfaces/Cryptography/Signature/IHmacSha1Generator.cs
@@ -2,6 +2,8 @@
 {
     public interface IHmacSha1Generator
     {
+        byte[] GenerateHmacSha1Hash(byte[] input, byte[] key);
+        byte[] GenerateHmacSha1Hash(long input, byte[] key);
         byte[] GenerateHmacSha1Hash(string input, byte[] key);
     }
 }
