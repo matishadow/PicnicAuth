ce8c757dba44926ad2e87db120f48beb16c498c2
matishadow <matishadow@gmail.com>
2017-08-18 17:11:17 +0200

    Fix tests

--- PicnicAuth/PicnicAuth.Tests/HmacSha1GeneratorTest.cs
+++ PicnicAuth/PicnicAuth.Tests/HmacSha1GeneratorTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using Moq;
 using NUnit.Framework;
 using PicnicAuth.Implementations.Cryptography.Hashing;
@@ -18,10 +19,14 @@ namespace PicnicAuth.Tests
         public void SetUp()
         {
             var mockULongConverter = new Mock<IULongConverter>();
+            mockULongConverter.Setup(p => p.ConvertToBytesBigEndian(0))
+                .Returns(Enumerable.Repeat<byte>(0, 8).ToArray());
+            mockULongConverter.Setup(p => p.ConvertToBytesBigEndian(100))
+                .Returns(new byte[] {0, 0, 0, 0, 0, 0, 0, 0x64});
 
             var mockUtf8Converter = new Mock<IUtf8Converter>();
-            mockUtf8Converter.Setup(p => p.ConvertToBytes("aaa")).Returns(new byte[] { 0x61, 0x61, 0x61 });
-            mockUtf8Converter.Setup(p => p.ConvertToBytes("ść")).Returns(new byte[] { 0xc5, 0x9b, 0xc4, 0x87 });
+            mockUtf8Converter.Setup(p => p.ConvertToBytes("aaa")).Returns(new byte[] {0x61, 0x61, 0x61});
+            mockUtf8Converter.Setup(p => p.ConvertToBytes("ść")).Returns(new byte[] {0xc5, 0x9b, 0xc4, 0x87});
             mockUtf8Converter.Setup(p => p.ConvertToBytes("")).Returns(new byte[] { });
 
             generator = new HmacSha1Generator(mockULongConverter.Object, mockUtf8Converter.Object);
@@ -75,16 +80,18 @@ namespace PicnicAuth.Tests
             return generator.GenerateHmacSha1Hash(input, key);
         }
 
-        [TestCase(100, ExpectedResult = new byte[]
-        {
-            0xd, 0x33, 0xd6, 0xf9, 0x18, 0x9, 0x78, 0x9b, 0x1a, 0x43, 0xf6, 0xcc, 0x9f, 0x6b, 0xa9, 0xbc, 0xc7, 0xd3,
-            0x58, 0xdc
-        })]
-        [TestCase(0, ExpectedResult = new byte[]
-        {
-            0x91, 0x4e, 0xc, 0x96, 0x34, 0x70, 0xc6, 0xd9, 0x3c, 0x13, 0x10, 0xaa, 0x2a, 0xee, 0x49, 0x88, 0xb1, 0xfb,
-            0xf1, 0xb0
-        })]
+        [TestCase(100U, new byte[] {0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x21, 0xde, 0xad, 0xbe, 0xef},
+            ExpectedResult = new byte[]
+            {
+                0x5a, 0xb7, 0x99, 0x78, 0x50, 0xf4, 0xe6, 0xf0, 0x40, 0xed, 0xd, 0x64, 0xce, 0x85, 0x7e, 0xe7, 0x8d,
+                0xd9, 0x5a, 0xf9
+            })]
+        [TestCase(ulong.MinValue, new byte[] {0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x21, 0xde, 0xad, 0xbe, 0xef},
+            ExpectedResult = new byte[]
+            {
+                0x2c, 0xd8, 0xe9, 0x4, 0xe3, 0x95, 0xa7, 0x41, 0x48, 0x77, 0xaa, 0x2f, 0x82, 0x61, 0xe1, 0x24, 0x35,
+                0x72, 0xc4, 0x35
+            })]
         public byte[] TestComputeStringHash(ulong input, byte[] key)
         {
             return generator.GenerateHmacSha1Hash(input, key);
