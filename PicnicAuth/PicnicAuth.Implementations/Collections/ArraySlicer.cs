e18a151c9e034266a38ec38947350dd3ce623086
matishadow <matishadow@gmail.com>
2017-08-21 12:56:45 +0200

    Add ArraySlicer stub

---
+++ PicnicAuth/PicnicAuth.Implementations/Collections/ArraySlicer.cs
@@ -0,0 +1,12 @@
+﻿using PicnicAuth.Interfaces.Collections;
+
+namespace PicnicAuth.Implementations.Collections
+{
+    public class ArraySlicer : IArraySlicer
+    {
+        public T[] Slice<T>(T[] array, int beginIndex, int endIndex)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
