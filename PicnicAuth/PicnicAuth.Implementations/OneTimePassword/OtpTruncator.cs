0308d1ca261880b09a8cf1c944626ea28e929cc3
matishadow <matishadow@gmail.com>
2017-08-21 12:14:54 +0200

    Add validating Sha1 length in OtpTruncator

--- PicnicAuth/PicnicAuth.Implementations/OneTimePassword/OtpTruncator.cs
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/OtpTruncator.cs
@@ -6,9 +6,13 @@ namespace PicnicAuth.Implementations.OneTimePassword
 {
     public class OtpTruncator : IOtpTruncator
     {
+        private const int ValidSha1SignatureLength = 20;
+
         public string Truncate(byte[] hmacSignature, OtpLength otpLength = OtpLength.SixDigits)
         {
             if (hmacSignature == null) throw new ArgumentNullException();
+            if (hmacSignature.Length != ValidSha1SignatureLength)
+                throw new ArgumentException(Properties.Resources.InvalidSha1LengthExceptionMessage);
 
             throw new NotImplementedException();
         }
--- PicnicAuth/PicnicAuth.Implementations/Properties/Resources.Designer.cs
+++ PicnicAuth/PicnicAuth.Implementations/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace PicnicAuth.Implementations.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -66,6 +66,15 @@ namespace PicnicAuth.Implementations.Properties {
         internal static string HmacSha1EmptyKeyExceptionMessage {
             get {
                 return ResourceManager.GetString("HmacSha1EmptyKeyExceptionMessage", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Sha1 signature must be of length 20..
+        /// </summary>
+        internal static string InvalidSha1LengthExceptionMessage {
+            get {
+                return ResourceManager.GetString("InvalidSha1LengthExceptionMessage", resourceCulture);
             }
         }
     }
