cc9ad3b1f428bb2aacf4f0462e5d2fd426bf4280
matishadow <matishadow@gmail.com>
2017-08-21 10:43:26 +0200

    Add stub truncator

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/OtpTruncator.cs
@@ -0,0 +1,13 @@
+﻿using PicnicAuth.Enums;
+using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class OtpTruncator : IOtpTruncator
+    {
+        public string Truncate(byte[] hmacSignature, OtpLength otpLength = OtpLength.SixDigits)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Interfaces/OneTimePassword/IOtpTruncator.cs
@@ -0,0 +1,9 @@
+﻿using PicnicAuth.Enums;
+
+namespace PicnicAuth.Interfaces.OneTimePassword
+{
+    public interface IOtpTruncator
+    {
+        string Truncate(byte[] hmacSignature, OtpLength otpLength = OtpLength.SixDigits);
+    }
+}
