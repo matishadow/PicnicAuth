7d775f722cdf0bf4d36a16807ef06c4f323fdc86
Matt Sawyer <matishadow@gmail.com>
2017-08-23 19:54:30 +0200

    Add TotpGenerator stub

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/TotpGenerator.cs
@@ -0,0 +1,12 @@
+﻿using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class TotpGenerator : ITotpGenerator
+    {
+        public string GenerateTotp(byte[] secret)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
