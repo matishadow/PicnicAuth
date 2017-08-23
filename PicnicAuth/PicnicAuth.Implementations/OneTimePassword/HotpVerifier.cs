2e175dff5c66db190dc8b099f7e14f10f6a1458f
Matt Sawyer <matishadow@gmail.com>
2017-08-23 20:45:07 +0200

    Add HotpVerifier stub

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/HotpVerifier.cs
@@ -0,0 +1,19 @@
+﻿using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class HotpVerifier : IHotpVerifier
+    {
+        private readonly IHotpGenerator generator;
+
+        public HotpVerifier(IHotpGenerator generator)
+        {
+            this.generator = generator;
+        }
+
+        public bool IsHotpValid(ulong counter, byte[] secret, string hotp)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
