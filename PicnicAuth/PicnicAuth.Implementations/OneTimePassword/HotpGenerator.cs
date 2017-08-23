f82bd4f48773b18fe4899e1e2eb0d4bdcbb02dac
Matt Sawyer <matishadow@gmail.com>
2017-08-23 18:30:41 +0200

    Add HotpGenerator stub

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/HotpGenerator.cs
@@ -0,0 +1,12 @@
+﻿using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class HotpGenerator : IHotpGenerator
+    {
+        public string GenerateHotp(ulong counter, byte[] secret)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
