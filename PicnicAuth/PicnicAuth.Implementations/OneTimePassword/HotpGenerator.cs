aca28817574da945c0290564b78f2060d9fe5673
Matt Sawyer <matishadow@gmail.com>
2017-08-23 19:37:49 +0200

    Add mocks and implementation

--- PicnicAuth/PicnicAuth.Implementations/OneTimePassword/HotpGenerator.cs
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/HotpGenerator.cs
@@ -1,12 +1,30 @@
-﻿using PicnicAuth.Interfaces.OneTimePassword;
+﻿using System;
+using PicnicAuth.Interfaces.Cryptography.Signature;
+using PicnicAuth.Interfaces.OneTimePassword;
 
 namespace PicnicAuth.Implementations.OneTimePassword
 {
     public class HotpGenerator : IHotpGenerator
     {
+        private readonly IHmacSha1Generator hmacSha1Generator;
+        private readonly IOtpTruncator otpTruncator;
+
+        public HotpGenerator(IHmacSha1Generator hmacSha1Generator, IOtpTruncator otpTruncator)
+        {
+            this.hmacSha1Generator = hmacSha1Generator;
+            this.otpTruncator = otpTruncator;
+        }
+
         public string GenerateHotp(ulong counter, byte[] secret)
         {
-            throw new System.NotImplementedException();
+            if (secret == null)
+                throw new ArgumentNullException();
+            if (secret.Length == byte.MinValue)
+                throw new ArgumentException();
+
+            byte[] hmac = hmacSha1Generator.GenerateHmacSha1Hash(counter, secret);
+            string otp = otpTruncator.Truncate(hmac);
+            return otp;
         }
     }
 }
