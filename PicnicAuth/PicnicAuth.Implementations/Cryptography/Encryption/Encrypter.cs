73dd73791741979efd81909d2995a96bd0be9afe
Matt Sawyer <matishadow@gmail.com>
2017-08-26 19:52:26 +0200

    Add stub Encrypter class

---
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Encryption/Encrypter.cs
@@ -0,0 +1,12 @@
+﻿using PicnicAuth.Interfaces.Cryptography.Encryption;
+
+namespace PicnicAuth.Implementations.Cryptography.Encryption
+{
+    public class Encrypter : IEncrypter
+    {
+        public byte[] Encrypt(byte[] bytesToEncrypt, byte[] key, byte[] salt, byte[] iv)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Interfaces/Cryptography/Encryption/IEncrypter.cs
@@ -0,0 +1,7 @@
+﻿namespace PicnicAuth.Interfaces.Cryptography.Encryption
+{
+    public interface IEncrypter
+    {
+        byte[] Encrypt(byte[] bytesToEncrypt, byte[] key, byte[] salt, byte[] iv);
+    }
+}
