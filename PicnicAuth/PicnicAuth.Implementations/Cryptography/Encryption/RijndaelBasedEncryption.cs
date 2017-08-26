eb050f27b3e8a866865b3485434e20e981e6a3ca
Matt Sawyer <matishadow@gmail.com>
2017-08-26 21:15:27 +0200

    Add RijndaelBaseEncryption base class

---
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Encryption/RijndaelBasedEncryption.cs
@@ -0,0 +1,8 @@
+﻿namespace PicnicAuth.Implementations.Cryptography.Encryption
+{
+    public abstract class RijndaelBasedEncryption
+    {
+        protected int KeySizeInBits => 256;
+        protected int KeySizeInBytes => KeySizeInBits / 8;
+    }
+}
