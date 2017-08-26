1def825b87074a0b9ff943037dbd4ed2430b87ab
Matt Sawyer <matishadow@gmail.com>
2017-08-26 22:34:23 +0200

    Add another test for KeyKerivation

--- PicnicAuth/PicnicAuth.Tests/KeyDerivationTest.cs
+++ PicnicAuth/PicnicAuth.Tests/KeyDerivationTest.cs
@@ -10,7 +10,7 @@ namespace PicnicAuth.Tests
     [TestFixture]
     public class KeyDerivationTest
     {
-        private static readonly byte[] ExamplePassword = { 0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x21, 0xde, 0xad, 0xbe, 0xef };
+        private static readonly byte[] ExamplePassword = {0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x21, 0xde, 0xad, 0xbe, 0xef};
         private const int ExampleIterations = 1000;
         private const int ExampleSizeInBytes = 256 / 8;
 
@@ -22,20 +22,32 @@ namespace PicnicAuth.Tests
             keyDerivation = new KeyDerivation();
         }
 
-        [Test]
-        public void TestGetDerivedBytes()
+        [TestCase(new byte[] {0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x21, 0xde, 0xad, 0xbe, 0xef},
+            new byte[] {0x48, 0x65, 0x6c, 0x6c, 0x6f, 0x21, 0xde, 0xad, 0xbe, 0xef},
+            ExampleIterations,
+            ExpectedResult = new byte[]
+            {
+                51, 69, 193, 85, 3, 111, 231, 204,
+                190, 42, 128, 143, 255, 161, 154, 160,
+                168, 112, 97, 172, 170, 9, 166, 237,
+                70, 188, 12, 117, 98, 169, 1, 34
+            })]
+        [TestCase(new byte[] {0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64},
+            new byte[] {0x70, 0x61, 0x73, 0x73, 0x77, 0x6f, 0x72, 0x64},
+            ExampleIterations,
+            ExpectedResult = new byte[]
+            {
+                0x95, 0x8f, 0x52, 0x68, 0xa3, 0x45, 0xc1, 0x9f,
+                0xd7, 0xd7, 0x13, 0xb9, 0x6, 0x6f, 0x4d, 0x6b,
+                0x2f, 0xd0, 0x80, 0x79, 0x33, 0xfa, 0x3a, 0xa9,
+                0x8a, 0x84, 0x95, 0x7f, 0x61, 0xff, 0x3a, 0xf2
+            })]
+        public byte[] TestGetDerivedBytes(byte[] password, byte[] salt, int iterations)
         {
             byte[] resultBytes = keyDerivation
-                .GetDerivedBytes(ExamplePassword, ExamplePassword, ExampleIterations, ExampleSizeInBytes);
+                .GetDerivedBytes(password, salt, iterations, ExampleSizeInBytes);
 
-            Assert.AreEqual(resultBytes,
-                new byte[]
-                {
-                    51, 69, 193, 85, 3, 111, 231, 204,
-                    190, 42, 128, 143, 255, 161, 154, 160,
-                    168, 112, 97, 172, 170, 9, 166, 237,
-                    70, 188, 12, 117, 98, 169, 1, 34
-                });
+            return resultBytes;
         }
 
         [Test]
@@ -56,8 +68,8 @@ namespace PicnicAuth.Tests
                 Throws.TypeOf<ArgumentException>());
         }
 
-        [TestCase(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, 0, ExampleIterations)]
-        [TestCase(new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, -100, ExampleIterations)]
+        [TestCase(new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 0, ExampleIterations)]
+        [TestCase(new byte[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, -100, ExampleIterations)]
         public void TestGetDerivedBytesInvalidArgumentOutOfRange(byte[] salt, int iterations, int keySizeInBytes)
         {
             Assert.That(
