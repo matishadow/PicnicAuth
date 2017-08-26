ac6b33d49f9d4597b9c17156699d3a676e9456df
Matt Sawyer <matishadow@gmail.com>
2017-08-26 17:41:29 +0200

    Add implementaions of TotpVerifier

--- PicnicAuth/PicnicAuth.Implementations/OneTimePassword/TotpVerifier.cs
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/TotpVerifier.cs
@@ -1,20 +1,49 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using PicnicAuth.Interfaces.OneTimePassword;
+using PicnicAuth.Interfaces.Time;
 
 namespace PicnicAuth.Implementations.OneTimePassword
 {
     public class TotpVerifier : ITotpVerifier
     {
-        private readonly ITotpGenerator generator;
+        private const int ValidTimeStepSize = 1;
+        private const int TotpTimeWindow = 30;
 
-        public TotpVerifier(ITotpGenerator generator)
+        private readonly IUnixTimestampGetter unixTimestampGetter;
+        private readonly IHotpGenerator hotpGenerator;
+
+        public TotpVerifier(IUnixTimestampGetter unixTimestampGetter, IHotpGenerator hotpGenerator)
         {
-            this.generator = generator;
+            this.unixTimestampGetter = unixTimestampGetter;
+            this.hotpGenerator = hotpGenerator;
         }
 
         public bool IsTotpValid(byte[] secret, string totp)
         {
-            throw new NotImplementedException();
+            if (secret == null || totp == null)
+                throw new ArgumentNullException();
+            if (secret.Length == byte.MinValue || string.IsNullOrWhiteSpace(totp))
+                throw new ArgumentException();
+
+            ulong currentTimestampInput = (ulong) unixTimestampGetter.GetUnixTimestamp() / TotpTimeWindow;
+            IEnumerable<ulong> consideredTimestampInputs = GetConsideredTimestampInputs(currentTimestampInput);
+
+            IEnumerable<string> totps = consideredTimestampInputs
+                .Select(input => hotpGenerator.GenerateHotp(input, secret));
+
+            return totps.Contains(totp);
+        }
+
+        private IEnumerable<ulong> GetConsideredTimestampInputs(ulong currentTimestampInput)
+        {
+            return new[]
+            {
+                currentTimestampInput,
+                currentTimestampInput - ValidTimeStepSize,
+                currentTimestampInput + ValidTimeStepSize
+            };
         }
     }
 }
