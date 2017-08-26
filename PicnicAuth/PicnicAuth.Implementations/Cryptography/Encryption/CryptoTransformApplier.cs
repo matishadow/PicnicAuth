f6a38754a6b81dd307baace44ad658da8b5d794a
Matt Sawyer <matishadow@gmail.com>
2017-08-26 21:06:27 +0200

    Add CryptoTransformApplier

---
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Encryption/CryptoTransformApplier.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Security.Cryptography;
+using PicnicAuth.Interfaces.Cryptography.Encryption;
+
+namespace PicnicAuth.Implementations.Cryptography.Encryption
+{
+    public class CryptoTransformApplier : ICryptoTransformApplier
+    {
+        public byte[] ApplyCryptoTransform(byte[] inputBytes, 
+            ICryptoTransform cryptoTransform, CryptoStreamMode streamMode)
+        {
+            MemoryStream memoryStream = null;
+            CryptoStream cryptoStream = null;
+
+            try
+            {
+                memoryStream = new MemoryStream();
+                cryptoStream = new CryptoStream(memoryStream, cryptoTransform, streamMode);
+
+                cryptoStream.Write(inputBytes, byte.MinValue, inputBytes.Length);
+                cryptoStream.FlushFinalBlock();
+
+                byte[] encryptedBytes = memoryStream.ToArray();
+                return encryptedBytes;
+            }
+            finally
+            {
+                memoryStream?.Dispose();
+                cryptoStream?.Dispose();
+            }
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Interfaces/Cryptography/Encryption/ICryptoTransformApplier.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Cryptography;
+
+namespace PicnicAuth.Interfaces.Cryptography.Encryption
+{
+    public interface ICryptoTransformApplier
+    {
+        byte[] ApplyCryptoTransform(byte[] inputBytes, 
+            ICryptoTransform cryptoTransform, CryptoStreamMode streamMode);
+    }
+}
