f99e37d46d38ebc0152e794962b088ba2faaaf19
Matt Sawyer <matishadow@gmail.com>
2017-08-26 16:12:52 +0200

    Add Totp verifyier stub

---
+++ PicnicAuth/PicnicAuth.Implementations/OneTimePassword/TotpVerifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using PicnicAuth.Interfaces.OneTimePassword;
+
+namespace PicnicAuth.Implementations.OneTimePassword
+{
+    public class TotpVerifier : ITotpVerifier
+    {
+        private readonly ITotpGenerator generator;
+
+        public TotpVerifier(ITotpGenerator generator)
+        {
+            this.generator = generator;
+        }
+
+        public bool IsTotpValid(byte[] secret, string totp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
