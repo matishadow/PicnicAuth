4541408ed1bc59aed9e49ee8724a6a1c9ecc7395
Matt Sawyer <matishadow@gmail.com>
2017-10-19 19:56:31 +0200

    Add AuthUser entity

---
+++ PicnicAuth/PicnicAuth.Database/Models/AuthUser.cs
@@ -0,0 +1,8 @@
+﻿namespace PicnicAuth.Database.Models
+{
+    public class AuthUser : Entity
+    {
+        public byte[] Secret { get; set; }
+        public string ExternalId { get; set; }
+    }
+}
--- PicnicAuth/PicnicAuth.Database/Models/Authentication/UserLoginInfoViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PicnicAuth.Database.Models.Authentication
-{
-    public class UserLoginInfoViewModel
-    {
-        public string LoginProvider { get; set; }
-
-        public string ProviderKey { get; set; }
-    }
-}
