7847bc82215951fb32f8d46120aa2312571b5049
Matt Sawyer <matishadow@gmail.com>
2017-10-19 20:47:06 +0200

    Add DPAPI usage

---
+++ PicnicAuth/PicnicAuth.Implementations/Cryptography/Encryption/DpapiDecryptor.cs
@@ -0,0 +1,17 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using PicnicAuth.Interfaces.Cryptography.Encryption;
+using PicnicAuth.ServiceInterfaces.Dependencies;
+
+namespace PicnicAuth.Implementations.Cryptography.Encryption
+{
+    public class DpapiDecryptor : IDpapiDecryptor, IRequestDependency
+    {
+        public string Decrypt(byte[] input, byte[] optionalEntropy = null,
+            DataProtectionScope scope = DataProtectionScope.CurrentUser)
+        {
+            byte[] bytes = ProtectedData.Unprotect(input, optionalEntropy, scope);
+            return System.Text.Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Interfaces/Cryptography/Encryption/IDpapiEncryptor.cs
@@ -0,0 +1,13 @@
+﻿using System.Security.Cryptography;
+
+namespace PicnicAuth.Interfaces.Cryptography.Encryption
+{
+    public interface IDpapiEncryptor
+    {
+        byte[] Encrypt(byte[] input, byte[] optionalEntropy = null,
+            DataProtectionScope scope = DataProtectionScope.CurrentUser);
+
+        byte[] Encrypt(string input, byte[] optionalEntropy = null,
+            DataProtectionScope scope = DataProtectionScope.CurrentUser);
+    }
+}
