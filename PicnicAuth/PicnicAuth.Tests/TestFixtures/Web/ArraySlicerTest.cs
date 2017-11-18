3a3e31be547acac614e60ef79a9ff42fd641106e
matishadow <matishadow@gmail.com>
2017-11-18 22:01:44 +0100

    Add test for UriGenerator

---
+++ PicnicAuth/PicnicAuth.Tests/TestFixtures/Web/ArraySlicerTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.Http;
+using NUnit.Framework;
+using PicnicAuth.Enums;
+using PicnicAuth.Implementations.Collections;
+using PicnicAuth.Implementations.Web;
+using PicnicAuth.Interfaces.Collections;
+using PicnicAuth.Interfaces.Web;
+
+namespace PicnicAuth.Tests.TestFixtures.Web
+{
+    [TestFixture]
+    public class OtpQrCodeUriGeneratorTest
+    {
+        private IOtpQrCodeUriGenerator uriGenerator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            uriGenerator = new OtpQrCodeUriGenerator();
+        }
+
+        [Test]
+        public void GenerateUri()
+        {
+            var message = new HttpRequestMessage(HttpMethod.Get, "http://someurl.com/awdaw/awd");
+            var userGuid = new Guid("4c32af93-3220-4b52-be8b-e9b6e1bdc852");
+            const string companyUserName = "aaabbbccc";
+
+
+            Uri resultUri = uriGenerator.GenerateQrCodeUri(OtpType.Totp, message, userGuid, companyUserName);
+
+            Assert.AreEqual(resultUri, 
+                new Uri("http://someurl.com/api/qrcodes/4c32af93-3220-4b52-be8b-e9b6e1bdc852?type=Totp&issuer=aaabbbccc"));
+        }
+    }
+}
