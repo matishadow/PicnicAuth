20e2fddee6bbf4c9be75610b851f2528afa7a5a9
matishadow <matishadow@gmail.com>
2017-11-18 21:09:47 +0100

    Add identity classes

---
+++ PicnicAuth/PicnicAuth.Models/Authentication/Identity/CompanyAccountRoleStore.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PicnicAuth.Models.Authentication.Identity
+{
+    public class CompanyAccountRoleStore : RoleStore<Role, Guid, CompanyAccountRole>
+    {
+        public CompanyAccountRoleStore(DbContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ PicnicAuth/PicnicAuth.Models/Authentication/Identity/CompanyAccountUserStore.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.Entity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace PicnicAuth.Models.Authentication.Identity
+{
+    public class CompanyAccountUserStore : UserStore<CompanyAccount, Role, Guid, CompanyAccountLogin, 
+        CompanyAccountRole, CompanyAccountClaim>
+    {
+        public CompanyAccountUserStore(DbContext context) : base(context)
+        {
+        }
+    }
+}
