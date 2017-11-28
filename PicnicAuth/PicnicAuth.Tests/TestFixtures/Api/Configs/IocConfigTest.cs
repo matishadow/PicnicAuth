ddb97a40a5eff6d5515754f8bf3032b5c405a62c
matishadow <matishadow@gmail.com>
2017-11-28 20:38:07 +0100

    Add attributes tests

---
+++ PicnicAuth/PicnicAuth.Tests/TestFixtures/Api/Configs/IocConfigTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using PicnicAuth.Api.Configs;
+
+namespace PicnicAuth.Tests.TestFixtures.Api.Configs
+{
+    [TestFixture]
+    public class IocConfigTest
+    {
+        public void TestRun()
+        {
+            Assert.DoesNotThrow(IocConfig.Run);
+        }
+    }
+}
