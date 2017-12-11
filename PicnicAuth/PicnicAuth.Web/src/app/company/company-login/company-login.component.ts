216adeba875035ce68086ac5d93e45216dea8564
matishadow <matishadow@gmail.com>
2017-12-11 23:13:57 +0100

    Display apiKEy better

--- PicnicAuth/PicnicAuth.Web/src/app/company/company-login/company-login.component.ts
+++ PicnicAuth/PicnicAuth.Web/src/app/company/company-login/company-login.component.ts
@@ -10,6 +10,7 @@ import { TokensApiService } from "../../tokens/tokens-api.service";
 export class CompanyLoginComponent implements OnInit {
   username: string;
   password: string;
+  apiKey: string;
 
   constructor(private notifierService: NotifierService,
     private tokensApiService: TokensApiService) { }
@@ -23,7 +24,8 @@ export class CompanyLoginComponent implements OnInit {
     this.tokensApiService
       .login(this.username, this.password)
       .subscribe(tokenResponse => {
-        this.notifierService.success(`API key: ${tokenResponse.access_token}`);
+        this.notifierService.success("API key successfully retrieved");
+        this.apiKey = tokenResponse.access_token;
       }, (error) => {
         this.notifierService.error(error);
       });
